c687d99c3b35c731b3794f0b26db181e19902619
GuidoMonstrey <guido.monstrey@telenet.be>
2019-06-11 16:26:16 +0200

    Update pi_code.py

--- pi_code.py
+++ pi_code.py
@@ -20,10 +20,10 @@ GPIO.output(LED, GPIO.LOW)
 
 
 # Twitter application authentication
-APP_KEY = 'YourTwitterInfoHere'
-APP_SECRET = 'YourTwitterInfoHere'
-OAUTH_TOKEN = 'YourTwitterInfoHere'
-OAUTH_TOKEN_SECRET = 'YourTwitterInfoHere'
+APP_KEY = pUw6yPihbG4TVAYcjVsPxRNPo
+APP_SECRET = JRkIwZ87RzmZ5bYvFMRw9wATV1zd3oIHG5snn3TDZR8yUolmnP
+OAUTH_TOKEN = 975697105-Xj5TUCM7leIgjhEKnDJpgo83e3VS96HUFfVbrPRY
+OAUTH_TOKEN_SECRET = djrU87A40unPdkYN8GPSIuHpjpxfGtF4gYz6l4MRTYzxl
 
 # Setup callbacks from Twython Streamer
 class BlinkyStreamer(TwythonStreamer):
